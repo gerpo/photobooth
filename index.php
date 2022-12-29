410492d033b81423e8862cc12f8bafe8adbb00ec
AdamMarciniak <adamarc@web.de>
2022-12-29 17:50:10 +0100

    WIP

--- index.php
+++ index.php
@@ -181,9 +181,9 @@ if (
         <?php endif; ?>
 
         <?php if ($config['aws']['upload_files']): ?>
-            <div id="s3DownloadCode" class="" style='position: fixed; bottom: 20px; right: 20px;'>
+            <div id="s3DownloadCode" class="" style='position: fixed; bottom: 20px; right: 20px; background-color: white; color: black'>
                 <img id="s3QRCodeImage" src='' alt='s3 download code' height='150px' width='150px'>
-                <p style='background-color: white; color: black'>Download</p>
+                <p>Download</p>
             </div>
         <?php endif; ?>
     </div>
